709e50aafa126a9a935b6d71e628e0ba5ff79b74
Mohan Reddy T <timmapuramreddy@gmail.com>
2022-06-30 07:10:46 +0530

    created feed component and its header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 
@@ -14,6 +15,7 @@ export default function Home() {
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
+        <Feed />
         {/* Widget Section */}
         {/* Modal */}
       </main>
